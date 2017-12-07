776d3d4b3afaf0ec4c5c7eded1752a6a083dfe09
Ian <152772a@mymail.nyp.edu.sg>
2017-12-08 11:01:03 +1300

    Changes

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var request = require('request');
+var querystring = require('querystring');
+var https = require('https');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -19,7 +22,7 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,52 +30,103 @@ app.use('/', index);
 app.use('/users', users);
 
 
-
-
-
 var http = require('http');
 var server = http.createServer(app);
 
+var SECRET = "6LdwBzwUAAAAAKavgcoL75Y4lF7QUQPKQyt_e6Qk";
 
-app.post('/loginBackend', function(req, res) {
+// POST request to google recaptcha
+function verifyRecaptcha(key, callback) {
+    console.log("RESPONSE IS: " + key)
+    //declare above var querystring = require('querystring') on top
+    var post_data = querystring.stringify({
+        'secret': SECRET,
+        'response': key
+    });
+
+    var post_options = {
+        host: 'www.google.com',
+        port: '443',
+        method: 'POST',
+        path: '/recaptcha/api/siteverify',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(post_data)
+        }
+    };
+    var req = https.request(post_options, function (res) {
+        var data = "";
+        res.on('data', function (chunk) {
+            data += chunk.toString();
+        });
+        res.on('end', function () {
+            try {
+                var parsedData = JSON.parse(data);
+                console.log("PARSED data "+data)
+                console.log("PARSED data "+parsedData)
+                callback(parsedData.success);
+            } catch (e) {
+                callback(false);
+            }
+        });
+    });
+    req.write(post_data);
+    req.end();
+    req.on('error', function (err) {
+        console.error(err);
+    });
+}
+
+
+app.post('/loginBackend', function (req, res) {
     console.log(req.body);
+
+
+    //https://codeforgeek.com/2016/03/google-recaptcha-node-js-tutorial/
+    if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+        return res.json({"responseCode": 1, "responseDesc": "Please select captcha"});
+    }
+
+    // Put your secret key here.
+
+
+    // Hitting GET request to the URL, Google will respond with success or error scenario.
+
+
+    verifyRecaptcha(req.body["g-recaptcha-response"], function (success) {
+        if (success) {
+            res.json({"responseCode": 0, "responseDesc": "Sucess"});
+        } else {
+            res.json({"responseCode": 1, "responseDesc": "Failed captcha verification"});
+        }
+    });
+
+
     // res.sendStatus(200);
     // res.render
-    res.render('index',{layout: 'layout/layout'});
+    // res.render('index',{layout: 'layout/layout'});
 });
 
 server.listen(process.env.PORT, process.env.IP);
 
 
-
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = app;
