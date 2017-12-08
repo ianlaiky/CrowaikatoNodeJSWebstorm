19e7a1ba9e4bfa72085b61795faed61ec0e2d7db
Ian <152772a@mymail.nyp.edu.sg>
2017-12-08 16:47:33 +1300

    Changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var querystring = require('querystring');
 var https = require('https');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var userAccounts = require('./routes/userAccounts');
 
 
 var app = express();
@@ -27,87 +27,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/users', userAccounts);
 
 
 var http = require('http');
 var server = http.createServer(app);
 
-var SECRET = "6LdwBzwUAAAAAKavgcoL75Y4lF7QUQPKQyt_e6Qk";
-
-// POST request to google recaptcha
-function verifyRecaptcha(key, callback) {
-    console.log("RESPONSE IS: " + key)
-    //declare above var querystring = require('querystring') on top
-    var post_data = querystring.stringify({
-        'secret': SECRET,
-        'response': key
-    });
-
-    var post_options = {
-        host: 'www.google.com',
-        port: '443',
-        method: 'POST',
-        path: '/recaptcha/api/siteverify',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': Buffer.byteLength(post_data)
-        }
-    };
-    var req = https.request(post_options, function (res) {
-        var data = "";
-        res.on('data', function (chunk) {
-            data += chunk.toString();
-        });
-        res.on('end', function () {
-            try {
-                var parsedData = JSON.parse(data);
-                console.log("PARSED data "+data)
-                console.log("PARSED data "+parsedData)
-                callback(parsedData.success);
-            } catch (e) {
-                callback(false);
-            }
-        });
-    });
-    req.write(post_data);
-    req.end();
-    req.on('error', function (err) {
-        console.error(err);
-    });
-}
 
 
-app.post('/loginBackend', function (req, res) {
-    console.log(req.body);
 
 
-    //https://codeforgeek.com/2016/03/google-recaptcha-node-js-tutorial/
-    if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
-        return res.json({"responseCode": 1, "responseDesc": "Please select captcha"});
-    }
-
-    // Put your secret key here.
 
 
-    // Hitting GET request to the URL, Google will respond with success or error scenario.
 
-
-    verifyRecaptcha(req.body["g-recaptcha-response"], function (success) {
-        if (success) {
-            res.json({"responseCode": 0, "responseDesc": "Sucess"});
-        } else {
-            res.json({"responseCode": 1, "responseDesc": "Failed captcha verification"});
-        }
-    });
-
-
-    // res.sendStatus(200);
-    // res.render
-    // res.render('index',{layout: 'layout/layout'});
-});
-
-server.listen(process.env.PORT, process.env.IP);
+// server.listen(process.env.PORT, process.env.IP);
 
 
 // catch 404 and forward to error handler
