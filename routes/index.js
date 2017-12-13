df16696bd0f1dccf4347000af0ea6ce3ac0d7cd0
Ian <152772a@mymail.nyp.edu.sg>
2017-12-13 16:46:23 +1300

    Changes

--- routes/index.js
+++ routes/index.js
@@ -6,49 +6,6 @@ var passport = require('passport')
 var router = express.Router();
 
 
-var SECRET = "6LdwBzwUAAAAAKavgcoL75Y4lF7QUQPKQyt_e6Qk";
-
-// POST request to google recaptcha
-function verifyRecaptcha(key, callback) {
-    console.log("RESPONSE IS: " + key);
-    //declare above var querystring = require('querystring') on top
-    var post_data = querystring.stringify({
-        'secret': SECRET,
-        'response': key
-    });
-
-    var post_options = {
-        host: 'www.google.com',
-        port: '443',
-        method: 'POST',
-        path: '/recaptcha/api/siteverify',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': Buffer.byteLength(post_data)
-        }
-    };
-    var req = https.request(post_options, function (res) {
-        var data = "";
-        res.on('data', function (chunk) {
-            data += chunk.toString();
-        });
-        res.on('end', function () {
-            try {
-                var parsedData = JSON.parse(data);
-                console.log("PARSED data " + data);
-                console.log("PARSED data " + parsedData);
-                callback(parsedData.success);
-            } catch (e) {
-                callback(false);
-            }
-        });
-    });
-    req.write(post_data);
-    req.end();
-    req.on('error', function (err) {
-        console.error(err);
-    });
-}
 
 
 /* GET home page. */
@@ -60,13 +17,6 @@ router.get('/', function (req, res, next) {
     console.log(messages.length>0);
     res.render('index', {layout: 'layout/layout',messages:messages,hasError:messages.length>0,success: req.session.success});
 });
-
-
-// router.post('/loginBackend',function (req, res, next){
-//     console.log("testwork")
-//     console.log(req.body)
-//
-// });
 
 
 
@@ -84,20 +34,6 @@ router.get('/register', function (req, res, next) {
     req.session.errors = null;
 });
 
-router.post("/registerForm", passport.authenticate('local.signup',{
 
-    successRedirect : '/register', // redirect to the secure profile section
-    failureRedirect : '/register', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-}));
-
-
-router.post('/loginBackend',passport.authenticate('local.signin',{
-
-    successRedirect : '/page', // redirect to the secure profile section
-    failureRedirect : '/', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-
-}));
 
 module.exports = router;
--- routes/signedInUsers.js
+++ routes/signedInUsers.js
@@ -1,29 +1,54 @@
-
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var request = require('request');
-var querystring = require('querystring');
-var https = require('https');
-
-var expressValidator = require('express-validator');
-var expressSession = require('express-session');
-
+var passport = require('passport');
 var router = express.Router();
 
 
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', isLoggedIn,function (req, res, next) {
     // res.render('index', { title: 'Express' });
     res.render('page/home', {layout: 'layout/layout'});
 });
 
+router.get('/logout',function (req, res, next) {
+    req.logout();
+    res.redirect("/")
+
+});
 
 
+
+router.post("/registerForm", passport.authenticate('local.signup',{
+
+    successRedirect : '/register', // redirect to the secure profile section
+    failureRedirect : '/register', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+}));
+
+
+router.post('/loginBackend',passport.authenticate('local.signin',{
+
+    successRedirect : '/page', // redirect to the secure profile section
+    failureRedirect : '/', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+
+}));
+
+function isLoggedIn(req,res,next){
+
+    if(req.isAuthenticated()){
+
+        return next();
+    }
+    res.redirect("/")
+
+
+
+
+
+
+
+}
 
 
 module.exports = router;
