475f0340e4303cf52cb17f4e6ba9a8231a443f47
Ian <152772a@mymail.nyp.edu.sg>
2017-12-13 17:12:33 +1300

    Changes

--- routes/index.js
+++ routes/index.js
@@ -20,19 +20,6 @@ router.get('/', function (req, res, next) {
 
 
 
-router.get('/register', function (req, res, next) {
-    // res.render('index', { title: 'Express' });
-
-
-    var messages = req.flash('error');
-    console.log(messages);
-    console.log(messages.length);
-    console.log(messages.length>0);
-
-
-    res.render('registration', {layout: 'layout/layout',messages:messages,hasError:messages.length>0,success: req.session.success});
-    req.session.errors = null;
-});
 
 
 
--- routes/signedInUsers.js
+++ routes/signedInUsers.js
@@ -5,9 +5,26 @@ var router = express.Router();
 
 
 /* GET users listing. */
-router.get('/', isLoggedIn,function (req, res, next) {
+router.get('/home', isLoggedIn,function (req, res, next) {
     // res.render('index', { title: 'Express' });
     res.render('page/home', {layout: 'layout/layout'});
+});
+
+
+
+
+router.get('/register', function (req, res, next) {
+    // res.render('index', { title: 'Express' });
+
+
+    var messages = req.flash('error');
+    console.log(messages);
+    console.log(messages.length);
+    console.log(messages.length>0);
+
+
+    res.render('registration', {layout: 'layout/layout',messages:messages,hasError:messages.length>0,success: req.session.success});
+    req.session.errors = null;
 });
 
 router.get('/logout',function (req, res, next) {
@@ -28,7 +45,7 @@ router.post("/registerForm", passport.authenticate('local.signup',{
 
 router.post('/loginBackend',passport.authenticate('local.signin',{
 
-    successRedirect : '/page', // redirect to the secure profile section
+    successRedirect : '/page/home', // redirect to the secure profile section
     failureRedirect : '/', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
 
@@ -41,11 +58,6 @@ function isLoggedIn(req,res,next){
         return next();
     }
     res.redirect("/")
-
-
-
-
-
 
 
 }
