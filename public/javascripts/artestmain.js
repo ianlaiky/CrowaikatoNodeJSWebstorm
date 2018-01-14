b48f949abfd5c9ee51629cdb82b5033d52d6a453
Ian <152772a@mymail.nyp.edu.sg>
2018-01-15 10:33:41 +1300

    artes

--- public/javascripts/artestmain.js
+++ public/javascripts/artestmain.js
@@ -3,19 +3,19 @@ function init() {
     var gui = new dat.GUI();
     var clock = new THREE.Clock();
 
-    var enableFog = false;
+    var enableFog = true;
 
     if (enableFog) {
-        scene.fog = new THREE.FogExp2(0xffffff, 0.2);
+        scene.fog = new THREE.FogExp2(0xffffff, 0.01);
     }
 
     var plane = getPlane(100);
     var directionalLight = getDirectionalLight(1);
     var sphere = getSphere(0.05);
-    var boxGrid = getBoxGrid(20,2.5);
-    boxGrid.name = 'boxGrid';
+    var boxGrid = getBoxGrid(20, 2.5);
 
     plane.name = 'plane-1';
+    boxGrid.name = 'boxGrid';
 
     plane.rotation.x = Math.PI/2;
     directionalLight.position.x = 13;
@@ -28,43 +28,66 @@ function init() {
     scene.add(directionalLight);
     scene.add(boxGrid);
 
-
-
-    // var camera = new THREE.PerspectiveCamera(
-    //     45,
-    //     window.innerWidth/window.innerHeight,
-    //     1,
-    //     1000
-    // );
-
-
     var camera = new THREE.PerspectiveCamera(
         45,
         window.innerWidth/window.innerHeight,
         1,
         1000
     );
+
+    var cameraZRotation = new THREE.Group();
     var cameraYPosition = new THREE.Group();
     var cameraZPosition = new THREE.Group();
-    var cameraYRotation = new THREE.Group();
     var cameraXRotation = new THREE.Group();
+    var cameraYRotation = new THREE.Group();
 
+    cameraZRotation.name = 'cameraZRotation';
     cameraYPosition.name = 'cameraYPosition';
     cameraZPosition.name = 'cameraZPosition';
     cameraXRotation.name = 'cameraXRotation';
     cameraYRotation.name = 'cameraYRotation';
 
-    cameraYPosition.add(camera);
+    cameraZRotation.add(camera);
+    cameraYPosition.add(cameraZRotation);
     cameraZPosition.add(cameraYPosition);
     cameraXRotation.add(cameraZPosition);
     cameraYRotation.add(cameraXRotation);
     scene.add(cameraYRotation);
 
-    cameraZPosition.position.z=100;
+    cameraXRotation.rotation.x = -Math.PI/2;
+    cameraYPosition.position.y = 1;
+    cameraZPosition.position.z = 100;
 
-    gui.add(cameraZPosition.position,'z',0,100);
-    gui.add(cameraYRotation.rotation,'y',-Math.PI,Math.PI);
-    gui.add(cameraXRotation.rotation,'x',-Math.PI,Math.PI);
+
+    new TWEEN.Tween({val:100})
+        .to({val:-50},12000)
+        .onUpdate(function () {
+            cameraZPosition.position.z=this.val;
+        })
+        .start();
+
+    new TWEEN.Tween({val:-Math.PI/2})
+        .to({val:0},6000)
+        .delay(1000)
+        .easing(TWEEN.Easing.Quadratic.InOut)
+        .onUpdate(function () {
+            cameraXRotation.rotation.x = this.val;
+        })
+        .start();
+
+    new TWEEN.Tween({val:0})
+        .to({val:Math.PI/2},6000)
+        .delay(1000)
+        .easing(TWEEN.Easing.Quadratic.InOut)
+        .onUpdate(function () {
+            cameraYRotation.rotation.y = this.val;
+        })
+        .start();
+
+    gui.add(cameraZPosition.position, 'z', 0, 100);
+    gui.add(cameraYRotation.rotation, 'y', -Math.PI, Math.PI);
+    gui.add(cameraXRotation.rotation, 'x', -Math.PI, Math.PI);
+    gui.add(cameraZRotation.rotation, 'z', -Math.PI, Math.PI);
 
     var renderer = new THREE.WebGLRenderer();
     renderer.shadowMap.enabled = true;
@@ -74,7 +97,7 @@ function init() {
 
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    update(renderer, scene, camera, controls,clock);
+    update(renderer, scene, camera, controls, clock);
 
     return scene;
 }
@@ -97,12 +120,12 @@ function getBoxGrid(amount, separationMultiplier) {
     var group = new THREE.Group();
 
     for (var i=0; i<amount; i++) {
-        var obj = getBox(1, 1, 1);
+        var obj = getBox(1, 3, 1);
         obj.position.x = i * separationMultiplier;
         obj.position.y = obj.geometry.parameters.height/2;
         group.add(obj);
         for (var j=1; j<amount; j++) {
-            var obj = getBox(1, 1, 1);
+            var obj = getBox(1, 3, 1);
             obj.position.x = i * separationMultiplier;
             obj.position.y = obj.geometry.parameters.height/2;
             obj.position.z = j * separationMultiplier;
@@ -166,39 +189,41 @@ function getDirectionalLight(intensity) {
     var light = new THREE.DirectionalLight(0xffffff, intensity);
     light.castShadow = true;
 
-    light.shadow.camera.left = -10;
-    light.shadow.camera.bottom = -10;
-    light.shadow.camera.right = 10;
-    light.shadow.camera.top = 10;
+    light.shadow.camera.left = -40;
+    light.shadow.camera.bottom = -40;
+    light.shadow.camera.right = 40;
+    light.shadow.camera.top = 40;
+
+    light.shadow.mapSize.width = 4096;
+    light.shadow.mapSize.height = 4096;
 
     return light;
 }
 
-function update(renderer, scene, camera, controls,clock) {
+function update(renderer, scene, camera, controls, clock) {
     renderer.render(
         scene,
         camera
     );
 
-    var timeElasped = clock.getElapsedTime();
+    controls.update();
+    TWEEN.update();
 
-    var cameraZPosition = scene.getObjectByName('cameraZPosition');
-    cameraZPosition.position.z-=0.25;
+    var timeElapsed = clock.getElapsedTime();
 
+
+    var cameraZRotation = scene.getObjectByName('cameraZRotation');
+    cameraZRotation.rotation.z = noise.simplex2(timeElapsed * 1.5, timeElapsed * 1.5) * 0.02;
 
     var boxGrid = scene.getObjectByName('boxGrid');
-    boxGrid.children.forEach((child,index)=>{
-        // console.log(Math.sin(timeElasped));
-        var x = timeElasped*5+index;
-        child.scale.y=(noise.simplex2(x,x)+1)/2+0.001;
-        child.position.y=child.scale.y/2;
+    boxGrid.children.forEach(function(child, index) {
+        var x = timeElapsed + index;
+        child.scale.y = (noise.simplex2(x, x) + 1) / 2 + 0.001;
+        child.position.y = child.scale.y/2;
     });
 
-
-    controls.update();
-
     requestAnimationFrame(function() {
-        update(renderer, scene, camera, controls,clock);
+        update(renderer, scene, camera, controls, clock);
     })
 }
 
