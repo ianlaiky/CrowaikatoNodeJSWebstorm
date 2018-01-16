2aaeb48dac0de72fbf530ca06122acbfe6f8673a
Ian <152772a@mymail.nyp.edu.sg>
2018-01-16 18:05:19 +1300

    ar laptop model

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var querystring = require('querystring');
 var https = require('https');
 var MongoDB = require('mongodb');
 
+
+
 var expressValidator = require('express-validator');
 var expressSession = require('express-session');
 
@@ -40,9 +42,116 @@ app.io = io;
 mongoose.Promise = Promise;
 
 // Connect to MongoDB
-// mongoose.connect('mongodb://192.168.204.129:27017/visualprogger', function(err) {
-//     if(err) throw err;
-// });
+mongoose.connect('mongodb://192.168.204.129:27017/ARDB', function(err) {
+    if(err) throw err;
+});
+
+var conn = mongoose.connection;
+
+
+
+
+
+
+
+login();
+
+///Ip Addresses of each marker (2 markers for this project)
+//IP of Red Team Machine (Offense)
+var redIp = "192.168.180.20";
+//IP of Blue Team Machine(Defense)
+var blueIp = "192.168.110.50";
+
+///Other Variables
+var srcIP, destIP;
+var min, max, random;
+
+// ///Create Instance of StitchClient
+// const client = new stitch.StitchClient('arcrow-lkurz');
+// const db = client.service('mongodb', 'mongodb-atlas').db('ARDB');
+
+///Anonymous Authentication and Start insertion of Data into DB
+function login() {
+    setInterval(randAction, 1500);
+}
+
+///Each of 12 Actions is Represented By a number from 0 to 12
+///This Function is to randomly select any action(number) for data insertion
+function randAction() {
+
+    min = Math.ceil(1);
+    max = Math.floor(12);
+    random = Math.floor(Math.random() * (max - min + 1)) + min;
+    var data = [];
+    data = getData(random);
+
+    //Insert into Db: Client Id, Action (represented by integer), Current DateTime, Source Ip & Dest Ip of action
+    // db.collection("ARaction").insert({
+    //     owner_id: client.authedId(),
+    //     actionType: data[0],
+    //     dateTime: new Date(),
+    //     src: data[1],
+    //     dest: data[2]
+    // }).then(console.log("data added!"));
+
+    var toinsert = {
+            owner_id: "1a168ad8058429ca16336c1b",
+            actionType: data[0],
+            dateTime: new MongoDB.Timestamp(0, Math.floor(new Date().getTime() / 1000)),
+            src: data[1],
+            dest: data[2]
+        };
+    conn.collection('ARaction').insert(toinsert);
+    //Display on paragraph
+    // document.getElementById("insert").innerHTML = data[0];
+    console.log("DB ADDDD");
+
+}
+
+
+
+//Each action is either an attack or a defense.
+//If is an attack, source Ip will be red machine and dest Ip will be blue machine
+//If is a defense, source Ip will be blue machine and dest Ip will be red machine
+function getData(type) {
+    var data = [];
+
+    if (type == 1 || type == 2 || type == 3 || type == 4 || type == 5 || type == 6 || type == 7 || type == 8 || type == 9) {
+
+        attack();
+
+    } else if (type == 10 || type == 11 || type == 12) {
+
+        defend();
+    } else {
+
+        console.log("error Input");
+        attack();
+    }
+
+    data.push(type);
+    data.push(srcIP);
+    data.push(destIP);
+
+    return data;
+
+}
+
+function attack() {
+    srcIP = redIp;
+    destIP = blueIp;
+}
+
+function defend() {
+    srcIP = blueIp;
+    destIP = redIp;
+}
+
+
+
+
+
+
 
 
 
--- socket/websocket.js
+++ socket/websocket.js
@@ -9,11 +9,12 @@ const File = mongoose.model("File");
 //AR
 var MongoDB = require('mongodb');
 ///Variables - Connection strings for MongoDB Atlas Databases
-var oplogurl = 'mongodb://tester:cR0w_%2B35t@arproject-shard-00-00-cjsdl.mongodb.net:27017,arproject-shard-00-01-cjsdl.mongodb.net:27017,' +
-    'arproject-shard-00-02-cjsdl.mongodb.net:27017/local?ssl=true&replicaSet=ARPROJECT-shard-0&authSource=admin';
+// var oplogurl = 'mongodb://tester:cR0w_%2B35t@arproject-shard-00-00-cjsdl.mongodb.net:27017,arproject-shard-00-01-cjsdl.mongodb.net:27017,' +
+//     'arproject-shard-00-02-cjsdl.mongodb.net:27017/local?ssl=true&replicaSet=ARPROJECT-shard-0&authSource=admin';
+
 var arurl = 'mongodb://tester:cR0w_+35t@arproject-shard-00-00-cjsdl.mongodb.net:27017,arproject-shard-00-01-cjsdl.mongodb.net:27017,' +
     'arproject-shard-00-02-cjsdl.mongodb.net:27017/ARDB?ssl=true&replicaSet=ARPROJECT-shard-0&authSource=admin';
-
+var oplogurl = 'mongodb://192.168.204.129:27017/ARDB';
 var machines = [];
 
 let clients = [];
@@ -24,20 +25,20 @@ var fileIntervalsArr = [];
 // call socket.io to the app
 let io = require('socket.io')();
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
     console.log('New client connected, SOCKETID: ' + socket.id);
 
     socket.on('disconnect', function () {
         clients[socket.id] = false;
-        for(var i = processIntervalsArr.length - 1; i >= 0; i--){
-          if(socket.id = processIntervalsArr[i]){
-            processIntervalsArr.splice(i, 1);
-          }
+        for (var i = processIntervalsArr.length - 1; i >= 0; i--) {
+            if (socket.id = processIntervalsArr[i]) {
+                processIntervalsArr.splice(i, 1);
+            }
         }
-        for(var i = fileIntervalsArr.length - 1; i >= 0; i--){
-          if(socket.id = processIntervalsArr[i]){
-            processIntervalsArr.splice(i, 1);
-          }
+        for (var i = fileIntervalsArr.length - 1; i >= 0; i--) {
+            if (socket.id = processIntervalsArr[i]) {
+                processIntervalsArr.splice(i, 1);
+            }
         }
         console.log('Client has disconnected, SOCKETID: ' + socket.id);
     });
@@ -52,26 +53,26 @@ io.on('connection', function(socket) {
      * Starts HERE
      */
     socket.on('scanProcess', function () {
-      // Repeated sending
-      let interval = setInterval(function () {
-          if (clients[socket.id] === false) {
-              clearInterval(interval);
-              return;
-          }
-          Process.findOne({}, 'scanId').sort('-scanId').exec(function (err, result) {
-
-            if (err) {
-                socket.emit('scanProcess', JSON.stringify({error: err.message}));
+        // Repeated sending
+        let interval = setInterval(function () {
+            if (clients[socket.id] === false) {
+                clearInterval(interval);
                 return;
             }
+            Process.findOne({}, 'scanId').sort('-scanId').exec(function (err, result) {
 
-            if (result === null) {
-                socket.emit('scanProcess', JSON.stringify({error: "Error or Nothing Found"}));
-                return;
-            }
-            socket.emit('scanProcess', result.scanId);
-        });
-      }, 500);
+                if (err) {
+                    socket.emit('scanProcess', JSON.stringify({error: err.message}));
+                    return;
+                }
+
+                if (result === null) {
+                    socket.emit('scanProcess', JSON.stringify({error: "Error or Nothing Found"}));
+                    return;
+                }
+                socket.emit('scanProcess', result.scanId);
+            });
+        }, 500);
     });
 
     socket.on('streamProcess', function (message) {
@@ -110,21 +111,23 @@ io.on('connection', function(socket) {
                 }
 
                 if (counter === result.counter) {
-                    console.log('your socketid is ' +socket.id);
-                    let count = processIntervalsArr.filter(function(value){return value === socket.id;}).length;
-                    if(count > 1){
-                        for(var i = processIntervalsArr.length - 1; i >= 0; i--){
-                          if(socket.id = processIntervalsArr[i]){
-                            processIntervalsArr.splice(i, 1);
-                            break;
-                          }
+                    console.log('your socketid is ' + socket.id);
+                    let count = processIntervalsArr.filter(function (value) {
+                        return value === socket.id;
+                    }).length;
+                    if (count > 1) {
+                        for (var i = processIntervalsArr.length - 1; i >= 0; i--) {
+                            if (socket.id = processIntervalsArr[i]) {
+                                processIntervalsArr.splice(i, 1);
+                                break;
+                            }
                         }
                     }
-                    if(count > 1){
-                      clearInterval(interval);
+                    if (count > 1) {
+                        clearInterval(interval);
                     }
-                    if(socket.id == undefined){
-                      clearInterval(interval);
+                    if (socket.id == undefined) {
+                        clearInterval(interval);
                     }
                     socket.emit('streamProcess', JSON.stringify({error: "Already Sent Counter ID " + result.counter}));
                 } else if (result !== null && (counter === null || counter < result.counter)) {
@@ -141,31 +144,31 @@ io.on('connection', function(socket) {
      */
 
 
-     /**
-      * FILE STREAM related METHODS
-      * Starts HERE
-      */
+    /**
+     * FILE STREAM related METHODS
+     * Starts HERE
+     */
     socket.on('scanFile', function () {
-      // Repeated sending
-      let interval = setInterval(function () {
-          if (clients[socket.id] === false) {
-              clearInterval(interval);
-              return;
-          }
-          File.findOne({}, 'scanId').sort('-scanId').exec(function (err, result) {
-
-            if (err) {
-                socket.emit('scanFile', JSON.stringify({error: err.message}));
+        // Repeated sending
+        let interval = setInterval(function () {
+            if (clients[socket.id] === false) {
+                clearInterval(interval);
                 return;
             }
+            File.findOne({}, 'scanId').sort('-scanId').exec(function (err, result) {
 
-            if (result === null) {
-                socket.emit('scanFile', JSON.stringify({error: "Error or Nothing Found"}));
-                return;
-            }
-            socket.emit('scanFile', result.scanId);
-        });
-      }, 500);
+                if (err) {
+                    socket.emit('scanFile', JSON.stringify({error: err.message}));
+                    return;
+                }
+
+                if (result === null) {
+                    socket.emit('scanFile', JSON.stringify({error: "Error or Nothing Found"}));
+                    return;
+                }
+                socket.emit('scanFile', result.scanId);
+            });
+        }, 500);
     });
 
     socket.on('streamFile', function (message) {
@@ -204,21 +207,23 @@ io.on('connection', function(socket) {
                 }
 
                 if (counter === result.counter) {
-                    console.log('your socketid is ' +socket.id);
-                    let count = fileIntervalsArr.filter(function(value){return value === socket.id;}).length;
-                    if(count > 1){
-                        for(var i = fileIntervalsArr.length - 1; i >= 0; i--){
-                          if(socket.id = fileIntervalsArr[i]){
-                            fileIntervalsArr.splice(i, 1);
-                            break;
-                          }
+                    console.log('your socketid is ' + socket.id);
+                    let count = fileIntervalsArr.filter(function (value) {
+                        return value === socket.id;
+                    }).length;
+                    if (count > 1) {
+                        for (var i = fileIntervalsArr.length - 1; i >= 0; i--) {
+                            if (socket.id = fileIntervalsArr[i]) {
+                                fileIntervalsArr.splice(i, 1);
+                                break;
+                            }
                         }
                     }
-                    if(count > 1){
-                      clearInterval(interval);
+                    if (count > 1) {
+                        clearInterval(interval);
                     }
-                    if(socket.id == undefined){
-                      clearInterval(interval);
+                    if (socket.id == undefined) {
+                        clearInterval(interval);
                     }
                     socket.emit('streamFile', JSON.stringify({error: "Already Sent Counter ID " + result.counter}));
                 } else if (result !== null && (counter === null || counter < result.counter)) {
@@ -235,72 +240,62 @@ io.on('connection', function(socket) {
      */
 
 
-
-
     /**
      * FILE STREAM related METHODS
      * Starts HERE
      */
     socket.on('scanTimestamp', function () {
-      // Repeated sending
-      let interval = setInterval(function () {
-           if (clients[socket.id] === false) {
-               clearInterval(interval);
-               return;
-           }
-           File.findOne({}).sort('-performanceTimestamp').exec(function (err, result) {
+        // Repeated sending
+        let interval = setInterval(function () {
+            if (clients[socket.id] === false) {
+                clearInterval(interval);
+                return;
+            }
+            File.findOne({}).sort('-performanceTimestamp').exec(function (err, result) {
 
-             if (err) {
-                 socket.emit('scanTimestamp', JSON.stringify({error: err.message}));
-                 return;
-             }
+                if (err) {
+                    socket.emit('scanTimestamp', JSON.stringify({error: err.message}));
+                    return;
+                }
 
-             if (result === null) {
-                 socket.emit('scanTimestamp', JSON.stringify({error: "Error or Nothing Found"}));
-                 return;
-             }
-             socket.emit('scanTimestamp', result);
-         });
-      }, 500);
+                if (result === null) {
+                    socket.emit('scanTimestamp', JSON.stringify({error: "Error or Nothing Found"}));
+                    return;
+                }
+                socket.emit('scanTimestamp', result);
+            });
+        }, 500);
     });
 
 
     //Connect to Oplog Collection and listen for insertion of data(actions)
-    MongoDB.MongoClient.connect(oplogurl, function(err, db) {
-
-        console.log("printttt");
-        console.log(oplogurl);
+    MongoDB.MongoClient.connect(oplogurl, function (err, db) {
 
 
-        if (err) {
+        db.collection('ARaction', (err, stuffInside) => {
 
-            console.log("conn error");
-            console.log(err);
-        }
-
-        db.collection('oplog.rs', function(err, oplog) {
             if (err) {
 
                 console.log("conn error");
                 console.log(err);
             }
-            oplog.find({}, {
-                ts: 1
+
+            console.log("initialal stuff");
+            console.log(stuffInside);
+
+            stuffInside.find({}, {
+                dateTime: 1
             }).sort({
                 $natural: -1
-            }).limit(1).toArray(function(err, data) {
-                console.log("WHat is this data");
-                console.log(data);
-                var lastOplogTime = data[0].ts;
-                console.log("--");
-                console.log(lastOplogTime);
-                console.log("MOngodb datatime");
-                console.log(MongoDB.Timestamp(0, Math.floor(new Date().getTime() / 1000)));
-                var queryForTime;
+            }).limit(1).toArray(function (err, data) {
 
-                if (lastOplogTime) {
+
+                console.log(data);
+                var lastTime = data[0].dateTime;
+
+                if (lastTime) {
                     queryForTime = {
-                        $gt: lastOplogTime
+                        $gt: lastTime
                     };
                 } else {
                     var tstamp = new MongoDB.Timestamp(0, Math.floor(new Date().getTime() / 1000))
@@ -308,29 +303,85 @@ io.on('connection', function(socket) {
                         $gt: tstamp
                     };
                 }
-                var cursor = oplog.find({
-                    ts: queryForTime
-                }, {
-                    tailable: true,
-                    awaitdata: true,
-                    oplogReplay: true,
-                    numberOfRetries: -1
-                });
-                console.log("loop");
-                var stream = cursor.stream();
-                stream.on('data', function(oplogdoc) {
-                    console.log("qn 2");
-                    console.log(oplogdoc);
-                    console.log(oplogdoc.ns);
-                    if (oplogdoc.ns == 'ARDB.ARaction') {
-                        socket.emit('action', oplogdoc);
-                    }
+console.log("STUFF INSIDE");
+console.log(stuffInside.find({}).toArray());
 
-                });
+
+                var cursor = stuffInside.find({},{
+                                dateTime: queryForTime
+                            });
+                console.log("CURSOR");
+                console.log(cursor);
             });
+
         });
 
+
+        //
+        // console.log("printttt");
+        // console.log(oplogurl);
+        //
+        //
+        // if (err) {
+        //
+        //     console.log("conn error");
+        //     console.log(err);
+        // }
+        //
+        // db.collection('oplog.rs', function(err, oplog) {
+        //     if (err) {
+        //
+        //         console.log("conn error");
+        //         console.log(err);
+        //     }
+        //     oplog.find({}, {
+        //         ts: 1
+        //     }).sort({
+        //         $natural: -1
+        //     }).limit(1).toArray(function(err, data) {
+        //         console.log("WHat is this data");
+        //         console.log(data);
+        //         var lastOplogTime = data[0].ts;
+        //         console.log("--");
+        //         console.log(lastOplogTime);
+        //         console.log("MOngodb datatime");
+        //         console.log(MongoDB.Timestamp(0, Math.floor(new Date().getTime() / 1000)));
+        //         var queryForTime;
+        //
+        //         if (lastOplogTime) {
+        //             queryForTime = {
+        //                 $gt: lastOplogTime
+        //             };
+        //         } else {
+        //             var tstamp = new MongoDB.Timestamp(0, Math.floor(new Date().getTime() / 1000))
+        //             queryForTime = {
+        //                 $gt: tstamp
+        //             };
+        //         }
+        //         var cursor = oplog.find({
+        //             ts: queryForTime
+        //         }, {
+        //             tailable: true,
+        //             awaitdata: true,
+        //             oplogReplay: true,
+        //             numberOfRetries: -1
+        //         });
+        //         console.log("loop");
+        //         var stream = cursor.stream();
+        //         stream.on('data', function(oplogdoc) {
+        //             console.log("qn 2");
+        //             console.log(oplogdoc);
+        //             console.log(oplogdoc.ns);
+        //             if (oplogdoc.ns == 'ARDB.ARaction') {
+        //                 socket.emit('action', oplogdoc);
+        //             }
+        //
+        //         });
+        //     });
+        // });
+
     });
-});
+})
+;
 
 module.exports = io;
