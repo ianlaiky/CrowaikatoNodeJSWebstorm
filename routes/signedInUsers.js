ce948694e5caf2b324b4a8ff4009da800f6b43af
Ian <152772a@mymail.nyp.edu.sg>
2018-02-02 15:18:20 +1300

    edited ti beautify code

--- routes/signedInUsers.js
+++ routes/signedInUsers.js
@@ -440,9 +440,9 @@ router.post("/homeSettingsDetailsEdit", isLoggedIn, (req, res, next) => {
 
 // change password/details
 router.get("/homeSettings", isLoggedIn, (req, res, next) => {
+    console.log("Home settings");
     let errormsgDetail = req.flash('errorHomeSettingDetail');
-    let passwordChangeSucc = req.flash('passw\n' +
-        '    console.log("Home setting");ordChangeSucc');
+    let passwordChangeSucc = req.flash('passwordChangeSucc');
     let detailschangesucc = req.flash('detailsChangeSucc');
     console.log(errormsgDetail);
     console.log(passwordChangeSucc);
