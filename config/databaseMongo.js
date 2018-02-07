5fe6b4caf869a2c7753cf0323deeeb5ec7f5b4c4
Ian <152772a@mymail.nyp.edu.sg>
2018-02-07 15:25:38 +1300

    comments

--- config/databaseMongo.js
+++ config/databaseMongo.js
@@ -1,6 +1,6 @@
 // File Created by: Ian Lai Kheng Yan
 
-// config/database.js
+
 module.exports = {
    'VisualProggerConnection':'mongodb://192.168.204.129:27017/visualprogger',
     'ARConnection':'mongodb://192.168.204.129:27017/ARDB'
